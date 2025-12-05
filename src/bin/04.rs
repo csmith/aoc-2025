11e5462323f12556c7ea8f314b940499211f7b87
Chris Smith <chris@chameth.com>
2025-12-05 14:52:39 +0000

    Day 4: even more optimisations

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,70 +1,77 @@
+use std::alloc::{alloc, Layout};
 use memmap2::Mmap;
 use std::fs::File;
 
 fn main() {
     let file = File::open("inputs/04.txt").expect("Couldn't open file");
-    let raw_grid: Vec<bool> = unsafe { Mmap::map(&file) }
-        .expect("Couldn't read file")
-        .iter()
-        .filter_map(|&b| match b {
-            b'@' => Some(true),
-            b'.' => Some(false),
-            _ => None,
-        })
-        .collect();
+    let bytes: Mmap = unsafe { Mmap::map(&file) }
+        .expect("Couldn't read file");
+    let size = bytes.len().isqrt();
+    let padded_size = size+2;
+    let total_size = padded_size * padded_size;
 
-    // Assuming all grids are square...
-    let width = raw_grid.len().isqrt();
-    let height = width;
+    let layout = Layout::array::<bool>(total_size * 2).unwrap();
+    let memory = unsafe { alloc(layout) as *mut bool };
+    let mut rolls: Vec<usize> = Vec::with_capacity(15000);
 
-    let mut grid: Vec<bool> = vec![false; (width+2)*(height+2)];
-    let mut grid_the_second: Vec<bool> = vec![false; (width+2)*(height+2)];
-    let mut rolls: Vec<(usize, usize)> = Vec::with_capacity(15000);
+    unsafe {
+        std::ptr::write_bytes(memory, 0, total_size * 2);
+    }
 
-    let mut source_y_offset = 0;
-    let mut dest_y_offset = width+2;
-    for y in 0..height {
-        for x in 0..width {
-            let val = raw_grid[source_y_offset+x];
-            grid[dest_y_offset+x+1] = val;
-            if val {
-                rolls.push((y, x))
+    let mut roll_count = 0;
+    let mut offset = padded_size + 1;
+    let mut x = 0;
+    for b in bytes.iter() {
+        match b {
+            b'@' => {
+                rolls.push(offset+x);
+                roll_count += 1;
+                unsafe {
+                    *memory.add(offset+x) = true;
+                }
+                x += 1;
+                if x == size {
+                    offset += padded_size;
+                    x = 0;
+                }
             }
+            b'.' => {
+                x += 1;
+                if x == size {
+                    offset += padded_size;
+                    x = 0;
+                }
+            }
+            _ => {}
         }
-        source_y_offset += width;
-        dest_y_offset += width + 2;
     }
 
     let mut first: bool = true;
-    let mut count: u32 = 0;
 
     // For the first iteration we want to make sure we're keeping the new
     // values separate from the old values, but for all future passes we
     // don't care. If we read a "new" value we'll potentially just remove
     // a roll sooner, which is a good thing.
-    let mut read_ptr = grid.as_ptr();
-    let write_ptr = grid_the_second.as_mut_ptr();
+    let mut read_ptr = memory;
+    let write_ptr = unsafe { memory.add(total_size) };
 
     loop {
         let mut removed = false;
 
         unsafe {
-            rolls.retain(|(y, x)| {
-                let index = (y + 1) * (width + 2) + x + 1;
-
+            rolls.retain(|&index| {
                 let neighbours = *read_ptr.add(index - 1) as u8
                     + *read_ptr.add(index + 1) as u8
-                    + *read_ptr.add(index - 1 - width - 2) as u8
-                    + *read_ptr.add(index + 1 - width - 2) as u8
-                    + *read_ptr.add(index - width - 2) as u8
-                    + *read_ptr.add(index - 1 + width + 2) as u8
-                    + *read_ptr.add(index + 1 + width + 2) as u8
-                    + *read_ptr.add(index + width + 2) as u8;
+                    + *read_ptr.add(index - 1 - padded_size) as u8
+                    + *read_ptr.add(index + 1 - padded_size) as u8
+                    + *read_ptr.add(index - padded_size) as u8
+                    + *read_ptr.add(index - 1 + padded_size) as u8
+                    + *read_ptr.add(index + 1 + padded_size) as u8
+                    + *read_ptr.add(index + padded_size) as u8;
 
                 let remove = neighbours < 4;
                 *write_ptr.add(index) = !remove;
                 removed |= remove;
-                count += remove as u32;
                 !remove
             })
         }
@@ -74,10 +81,12 @@ fn main() {
         }
 
         if first {
-            read_ptr = grid_the_second.as_ptr();
+            read_ptr = write_ptr;
+            let count = roll_count - rolls.len();
             println!("{count}");
             first = false;
         }
     }
+    let count = roll_count - rolls.len();
     println!("{count}")
 }
