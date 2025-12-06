34c09828568fccf84f8e79a135c79a786b44037f
Chris Smith <chris@chameth.com>
2025-12-06 20:26:44 +0000

    Ugly but working day 6

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,57 +1,45 @@
-use aoc_2025::atoi_u64;
-
 fn main() {
     let file = std::fs::read_to_string("inputs/06.txt").expect("Couldn't read file");
-    let mut bits: Vec<Vec<&str>> = Vec::new();
-    file.lines().for_each(|l| {
-        l.split_whitespace().enumerate().for_each(|(i, bit)| {
-            if bits.len() <= i {
-                bits.push(Vec::new())
+    let lines: Vec<Vec<u8>> = file.lines().map(|s| s.bytes().collect()).collect();
+
+    let mut part_one = 0u64;
+    let mut part_two = 0u64;
+    let mut lefties: Vec<u64> = vec![0u64; 4];
+    let mut downies: Vec<u64> = vec![0u64; 4];
+    let mut downy_count = 0;
+    let mut operator_char = b'?';
+
+    for i in 0..=lines[0].len() {
+        let gap = i == lines[0].len() || lines.iter().all(|line| line[i] == b' ');
+        if gap {
+            let operator = match operator_char {
+                b'+' => |v: Vec<u64>| v.iter().sum(),
+                b'*' => |v: Vec<u64>| v.iter().fold(1u64, |acc, v| acc * v),
+                _ => panic!("Don't know how to {operator_char}"),
+            };
+            part_one += operator(lefties.clone());
+            part_two += operator(downies.iter().take(downy_count).map(|&u| u).collect());
+            lefties.fill(0u64);
+            downies.fill(0u64);
+            downy_count = 0;
+            continue
+        }
+
+        for x in 0..4 {
+            let char = lines[x][i];
+            if char != b' ' {
+                lefties[x] = lefties[x] * 10 + (char - b'0') as u64;
+                downies[downy_count] = downies[downy_count] * 10 + (char - b'0') as u64
             }
-            bits[i].push(bit);
-        })
-    });
+        }
 
-    let (part_one, part_two): (u64, u64) =
-        bits.iter()
-            .fold((0u64, 0u64), |(part_one, part_two), parts| {
-                let op_str = parts.last().unwrap();
-                let op = match *op_str {
-                    "*" => |values: Vec<&str>| {
-                        values
-                            .iter()
-                            .map(|&v| atoi_u64(v))
-                            .fold(1u64, |acc, v| acc * v)
-                    },
-                    "+" => |values: Vec<&str>| {
-                        values
-                            .iter()
-                            .map(|&v| atoi_u64(v))
-                            .fold(0u64, |acc, v| acc + v)
-                    },
-                    _ => panic!("Unknown op {op_str}"),
-                };
+        if lines[4][i] != b' ' {
+            operator_char = lines[4][i];
+        }
 
-                // "What's the design goal for this programming language?"
-                // "IDK, how about making trivial string manipulation rage-inducing for some reason?"
-                let terms: Vec<&str> = parts.iter().take(parts.len() - 1).copied().collect();
-                let max_length = terms.iter().map(|x| x.len()).max().unwrap();
-                // This is wrong, they shouldn't be padded, it needs to be aware of the arbitrary whitespace in the input,
-                // which means rewriting how the parsing works, but this is not fun in the slightest.
-                let padded_terms: Vec<String> = terms.iter().map(|&x| format!("{:>width$}", x, width=max_length)).collect();
-                let transposed_terms: Vec<String> = padded_terms.iter().fold(vec![String::new(); max_length], |mut vec, term| {
-                    term.bytes().enumerate().for_each(|(i, c)| {
-                        vec[i].push(c as char);
-                    });
-                    vec
-                });
-                let transposed_terms_stripped: Vec<&str> = transposed_terms.iter().map(|s| s.trim()).collect();
+        downy_count += 1;
+    }
 
-                (
-                    part_one + op(terms),
-                    part_two + op(transposed_terms_stripped),
-                )
-            });
     println!("{part_one}");
     println!("{part_two}");
 }
