fccceaf81d580c9c194e6700439602d2faea1f93
Chris Smith <chris@chameth.com>
2025-12-06 10:28:50 +0000

    Day 4: another new algorithm I thought of in bed

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,33 +1,32 @@
-use std::alloc::{alloc, Layout};
 use memmap2::Mmap;
+use std::alloc::{Layout, alloc};
 use std::fs::File;
+
+const ROLL_PRESENT: i8 = 0b00100000;
+const ROLL_NEWLY_REMOVABLE: i8 = 0b00100011;
 
 fn main() {
     let file = File::open("inputs/04.txt").expect("Couldn't open file");
-    let bytes: Mmap = unsafe { Mmap::map(&file) }
-        .expect("Couldn't read file");
+    let bytes: Mmap = unsafe { Mmap::map(&file) }.expect("Couldn't read file");
     let size = bytes.len().isqrt();
-    let padded_size = size+2;
+    let padded_size = size + 2;
     let total_size = padded_size * padded_size;
 
-    let layout = Layout::array::<bool>(total_size * 2).unwrap();
-    let memory = unsafe { alloc(layout) as *mut bool };
-    let mut rolls: Vec<usize> = Vec::with_capacity(15000);
+    let layout = Layout::array::<i8>(total_size).unwrap();
+    let memory = unsafe { alloc(layout) as *mut i8 };
+    let mut pending_removals: Vec<usize> = Vec::with_capacity(1750);
 
     unsafe {
-        std::ptr::write_bytes(memory, 0, total_size * 2);
+        std::ptr::write_bytes(memory, 0, total_size);
     }
 
-    let mut roll_count = 0;
     let mut offset = padded_size + 1;
     let mut x = 0;
     for b in bytes.iter() {
         match b {
             b'@' => {
-                rolls.push(offset+x);
-                roll_count += 1;
                 unsafe {
-                    *memory.add(offset+x) = true;
+                    *memory.add(offset + x) |= ROLL_PRESENT;
                 }
                 x += 1;
                 if x == size {
@@ -46,47 +45,62 @@ fn main() {
         }
     }
 
-    let mut first: bool = true;
-
-    // For the first iteration we want to make sure we're keeping the new
-    // values separate from the old values, but for all future passes we
-    // don't care. If we read a "new" value we'll potentially just remove
-    // a roll sooner, which is a good thing.
-    let mut read_ptr = memory;
-    let write_ptr = unsafe { memory.add(total_size) };
-
-    loop {
-        let mut removed = false;
-
+    // First pass: mark all the rolls with <4 neighbours, which also happens
+    // to get us the part one answer
+    for index in 0..total_size {
         unsafe {
-            rolls.retain(|&index| {
-                let neighbours = *read_ptr.add(index - 1) as u8
-                    + *read_ptr.add(index + 1) as u8
-                    + *read_ptr.add(index - 1 - padded_size) as u8
-                    + *read_ptr.add(index + 1 - padded_size) as u8
-                    + *read_ptr.add(index - padded_size) as u8
-                    + *read_ptr.add(index - 1 + padded_size) as u8
-                    + *read_ptr.add(index + 1 + padded_size) as u8
-                    + *read_ptr.add(index + padded_size) as u8;
-
-                let remove = neighbours < 4;
-                *write_ptr.add(index) = !remove;
-                removed |= remove;
-                !remove
-            })
-        }
-
-        if !removed {
-            break;
-        }
-
-        if first {
-            read_ptr = write_ptr;
-            let count = roll_count - rolls.len();
-            println!("{count}");
-            first = false;
+            let value = *memory.add(index);
+            if roll_present(value) {
+                let neighbours = roll_present(*memory.add(index - 1)) as i8
+                    + roll_present(*memory.add(index + 1)) as i8
+                    + roll_present(*memory.add(index - 1 - padded_size)) as i8
+                    + roll_present(*memory.add(index + 1 - padded_size)) as i8
+                    + roll_present(*memory.add(index - padded_size)) as i8
+                    + roll_present(*memory.add(index - 1 + padded_size)) as i8
+                    + roll_present(*memory.add(index + 1 + padded_size)) as i8
+                    + roll_present(*memory.add(index + padded_size)) as i8;
+                *memory.add(index) = value + neighbours;
+                if neighbours < 4 {
+                    pending_removals.push(index);
+                }
+            }
         }
     }
-    let count = roll_count - rolls.len();
-    println!("{count}")
+
+    let part_one = pending_removals.len();
+    println!("{part_one}");
+
+    let mut part_two = 0;
+
+    // Remainder: for each pending removal, decrement all the neighbours and
+    // see if they're now eligible for removal
+    while let Some(index) = pending_removals.pop() {
+        notify_neighbour_of_removal(memory, &mut pending_removals, index - 1);
+        notify_neighbour_of_removal(memory, &mut pending_removals, index + 1);
+        notify_neighbour_of_removal(memory, &mut pending_removals, index - 1 - padded_size);
+        notify_neighbour_of_removal(memory, &mut pending_removals, index + 1 - padded_size);
+        notify_neighbour_of_removal(memory, &mut pending_removals, index - padded_size);
+        notify_neighbour_of_removal(memory, &mut pending_removals, index - 1 + padded_size);
+        notify_neighbour_of_removal(memory, &mut pending_removals, index + 1 + padded_size);
+        notify_neighbour_of_removal(memory, &mut pending_removals, index + padded_size);
+        part_two += 1;
+    }
+
+    println!("{part_two}")
+}
+
+#[inline(always)]
+fn notify_neighbour_of_removal(memory: *mut i8, pending_removals: &mut Vec<usize>, index: usize) {
+    unsafe {
+        let pointer = memory.add(index);
+        *pointer -= 1;
+        if *pointer == ROLL_NEWLY_REMOVABLE {
+            pending_removals.push(index);
+        }
+    }
+}
+
+#[inline(always)]
+fn roll_present(value: i8) -> bool {
+    value & ROLL_PRESENT == ROLL_PRESENT
 }
