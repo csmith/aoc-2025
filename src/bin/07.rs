b4fd261a28e02a5cebf686c22d4f666dd062ddf6
Chris Smith <chris@chameth.com>
2025-12-08 01:08:01 +0000

    Day 7 optimisations

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -3,41 +3,51 @@ use std::alloc::{Layout, alloc};
 use std::fs::File;
 
 fn main() {
+    const WIDTH: usize = 141;
+    const START: usize = 70;
+
     let file = File::open("inputs/07.txt").expect("Couldn't open file");
     let bytes: Mmap = unsafe { Mmap::map(&file) }.expect("Couldn't read file");
-    let lines: Vec<&[u8]> = bytes.split(|&b| b == b'\n').collect();
-    let width = lines[0].len();
 
-    let layout = Layout::array::<u64>(width + 2).unwrap();
+    let layout = Layout::array::<u64>(WIDTH).unwrap();
     let beams = unsafe { alloc(layout) as *mut u64 };
     unsafe {
-        std::ptr::write_bytes(beams, 0, width);
+        std::ptr::write_bytes(beams, 0, WIDTH);
+        *beams.add(START + 1) = 1;
     }
 
     let mut part_one = 0u64;
-
-    lines.iter()
-        .enumerate()
-        .filter(|(i, _)| i % 2 == 0)
-        .map(|(_, v)| v)
-        .for_each(|l| {
-        l.iter().enumerate().for_each(|(i, v)| match v {
-            b'S' => unsafe { *beams.add(i + 1) += 1 },
-            b'^' => unsafe {
-                let value = *beams.add(i + 1);
-                if value != 0 {
+    let mut line_offset = (WIDTH + 1) * 2;
+    let mut left_bound = START;
+    let mut right_bound = START;
+    while line_offset < bytes.len() {
+        // HERE YE, HERE YE, LET IT BE KNOWN THAT WE WILL BE READING THIS RANGE OF BYTES
+        // For some reason this performs better reading the ~full line, trying to limit
+        // it to left-bound...right-bound is slower. Not doing this at all is also slower.
+        // Computers... How do they work?
+        let line = &bytes[line_offset..line_offset + WIDTH];
+        let mut i = left_bound;
+        while i <= right_bound {
+            if line[i] == b'^' {
+                unsafe {
+                    let value = *beams.add(i + 1);
                     *beams.add(i + 1) = 0;
                     *beams.add(i) += value;
                     *beams.add(i + 2) += value;
-                    part_one += 1;
+                    part_one += (value != 0) as u64;
+                    i += 1; // Never another ^ next to us, don't bother checking
                 }
-            },
-            _ => {}
-        });
-    });
+            }
+            i += 1;
+        }
+        // Skip the next line as well, there's always a blank one
+        line_offset += (WIDTH + 1) * 2;
+        left_bound -= 1;
+        right_bound += 1;
+    }
 
     // Where does the one come from? Answers on a postcard...
-    let part_two: u64 = 1 + (0..width).map(|i| unsafe { *beams.add(i) }).sum::<u64>();
+    let part_two: u64 = 1 + (0..WIDTH).map(|i| unsafe { *beams.add(i) }).sum::<u64>();
     println!("{part_one}");
     println!("{part_two}");
 }
